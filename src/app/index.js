0597f9383f3b620638fc40889573d833838009a6
Márcio Viegas <marcioaugustoviegas@gmail.com>
2015-06-09 18:10:03 -0300

    fetch periods

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('deliveryDashboardUi', ['ngAnimate', 'restangular', 'ui.router', 'mgcrea.ngStrap'])
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, RestangularProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
+
+    RestangularProvider.setDefaultHeaders({
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest'
+    });
 
     $urlRouterProvider.otherwise('/');
   })
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,9 +1,11 @@
-'use strict';
 
-angular.module('deliveryDashboardUi')
-  .controller('MainCtrl', function () {
+    'use strict';
 
+    angular.module('deliveryDashboardUi').controller('MainCtrl', function ($scope, Restangular) {
 
+      Restangular.setBaseUrl("http://localhost:3000/api")
 
+      $scope.periods = Restangular.all('periods').getList().$object;
 
-  });
+    });
+
